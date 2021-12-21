8e14b1bc70eb365cff2342aa8b9a0ecf7d5d5370
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2021-12-21 10:00:32 +0300

    Add files via upload

---
+++ src/YeniProjeSirinler/Dusman.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package YeniProjeSirinler;
+
+
+public class Dusman extends Karakter{
+    int dusmanID;
+    String dusmanAdi = "";
+    String dusmanTur = "";
+    
+    
+    //GETTER VE SETTER
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String getAd() {
+        return ad;
+    }
+
+    @Override
+    public String getTur() {
+        return tur;
+    }
+
+    @Override
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    @Override
+    public void setTur(String tur) {
+        this.tur = tur;
+    }
+    
+    
+}
+    
+    
